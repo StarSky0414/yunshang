1ace0cfc03aea11ce8916d68bdc1d3d7cb8ac83d
StarSky <254143170@qq.com>
2019-05-27 19:29:29 +0800

    【修改】自动备份规则修改

--- app/src/main/java/com/tts/starsky/phonesweepcode/db/BackUp.java
+++ app/src/main/java/com/tts/starsky/phonesweepcode/db/BackUp.java
@@ -1,9 +1,10 @@
 package com.tts.starsky.phonesweepcode.db;
 
 import android.content.Context;
-
 import com.tts.starsky.phonesweepcode.oss.UpFile;
+import com.tts.starsky.phonesweepcode.utile.OkHttpUtil;
 import com.tts.starsky.phonesweepcode.utile.SharedPreferencesUtil;
+import java.text.SimpleDateFormat;
 
 /**
  * 数据库备份使用
@@ -12,7 +13,7 @@ public class BackUp {
 
     private final Context context;
 
-    public BackUp(Context context){
+    public BackUp(Context context) {
         this.context = context;
     }
 
@@ -24,11 +25,11 @@ public class BackUp {
         long backupSqlDay = timeMilliToDay(backupSqlDate);
         long timeMillisDay = timeMilliToDay(timeMillis);
 
-        if (backupSqlDay ==0 || backupSqlDay < timeMillisDay) {
+        if (backupSqlDay == 0 || backupSqlDay < timeMillisDay) {
             String absolutePath = context.getApplicationContext().getDatabasePath("my.db").getAbsolutePath();
             UpFile upFile = new UpFile();
-            upFile.upfile(absolutePath,"yunshang/backup/"+timeMillisDay);
-            System.out.println("==========="+absolutePath);
+            upFile.upfile(absolutePath, "yunshang/backup/" + timeMillisDay);
+            System.out.println("===========" + absolutePath);
             sharedPreferencesUtil.setBackupSqlDate();
         }
     }
@@ -37,7 +38,27 @@ public class BackUp {
      * 将时间戳转换为日期时间
      */
     private static long timeMilliToDay(long timeMillis) {
-        long dayNum = timeMillis / 3600 / 24;
+        String date = timeStamp2Date(timeMillis);
+        long dayNum = Long.valueOf(date);
         return dayNum;
     }
+
+    private static String timeStamp2Date(long timeMillis) {
+        String format = "yyyyMMdd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(timeMillis);
+    }
+
+    /**
+     * 数据恢复暂未启用
+     *
+     * @param childUrl 文件名
+     */
+    public void recoverDB(String childUrl) {
+        OkHttpUtil okHttpUtil = new OkHttpUtil();
+        String absolutePath = context.getApplicationContext().getDatabasePath("my.db").getAbsolutePath();
+        String urlRoot = "https://thethreestooges.oss-cn-shenzhen.aliyuncs.com/yunshang/backup/";
+        okHttpUtil.downFile(urlRoot + childUrl, absolutePath);
+    }
+
 }
